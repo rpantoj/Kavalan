0d19ef4688b505cdb62dfdf92be5a6f0f99769ae
Ariel Barboza <36430592+xarielx@users.noreply.github.com>
2019-12-03 23:32:23 -0500

    Added juventus Images

--- images.js
+++ images.js
@@ -18,7 +18,343 @@ const data = [
     {
         path: "./Images/messi2.gif",
         tags: ["messi"]
-    }
+    },
+    {
+      path: "./Images/1.5de6407efd9db2486d1b2697.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/10.juventus-rise.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/11.JuventusWomenCrop.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/12.20190311_Can-Juventus-FC_-lead-by-Cristiano-Ronaldo_-beat-Atletico.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/13.https%3A%2F%2Fhypebeast.com%2Fimage%2F2019%2F11%2Fpalace-juventus-collaboration-industry-insider-reactions-TW.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/14.demiral.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/15.Champions-League-Juventus-Manchester-City-reach-quarterfinals.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/16.Juventus.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/17.PES_2020_Juve_Banner_Image.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/18.AP19314784917031.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/19.AP190718108140.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/2.Juventus-Cristiano-Ronaldo-ends-scoring-drought-vs-Sassuolo.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/20.Cristiano-Ronaldo-Juventus-AP.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/21.https%3A%2F%2Fhypebeast.com%2Fimage%2F2019%2F10%2Ftw-palace-skateboards-juventus-adidas-football-2019-20-collaboration-release-info.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/22._hes9428_h.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/23.10xBet-Juventus-official-sponsorship.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/24.TeamHeader-Juventus.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/25.Ancelotti-Juventus-FootyAnalyst.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/26.MandzukicJuve.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/27._102479277_0581797a-1eee-47da-81c3-975f093b814e.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/28.juventus-home-kit-history.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/29.1163968928-1-scaled-746x372.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/3.singer-rihanna-with-juventus-t-shirt-attends-the-uefa-news-photo-1575135513.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/30.higuain_juventus_crop1575303870457.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/31.2019%2F7%2Fmoise30.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/32.ss-lazio-vs-juventus-live-stream-serie-a.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/33.FTO-OPEN-Blog-1200x600.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/34.000_1ML8LM-600x237.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/35.juve_w.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/36.5ddd9f65d8ad58130db81d2c.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/37.Cristiano-Ronaldo-Juventus-1280.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/38.vjluFjqpr_1256x620__1.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/39.Champions-League-Barcelona-tops-United-Juventus-plays-to-draw-with-Ajax.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/4.paulo_dybala_juventus_ac_milan.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/40.2009-03-10T220306Z_1_MT1ACI5814479_RTRMADP_3_SOCCER-CHAMPIONS-JUV-CHE.JPG",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/41.juventus-sanantonio-summer-clinic-2.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/42.ICC-2019_Stockholm_Atletico-de-Madrid-V-Juventusstockholmlive_1000X500-4c0509384d.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/43.https%3A%2F%2Fhypebeast.com%2Fimage%2F2019%2F10%2Ftw-juventus-palace-adidas-football-kit-official-unveil.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/44.2019-09-11_JUVENTUSvsFCB_22-min.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/45.1575206384-lapresse-20191201131857-31267239.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/45.Juventus%20goal%20celebration%20vs.%20Bayern.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/46.1575206384-lapresse-20191201131857-31267239.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/46.Juventus%20goal%20celebration%20vs.%20Bayern.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/47.s3-news-tmp-136742-s3-news-tmp-111981-juventus_stadium-2x1-940--2x1--940.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/48.euro-footy-focus-is-llorente-the-man-to-take-juventus-forward.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/49.can-ronaldo-lure-pogba-back-to-juventus-1132x509.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/5.CR7Atleti.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/50.juventus-vs-sassuolo_88da317.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/51.juventus--e1571778148709.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/52.GOLF_20191201140925786-1280x640.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/53.juventus-book-champions-league-final-place.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/54.USATSI_11008206.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/55.Dybala-1200.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/56.Ligue-des-Champions-au-Juventus-Stadium-l-Ajax-reve-d-un-nouvel-exploit.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/57.fifa-20-juventus-rights-players-team-name-situation-1200x450.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/58.MorataATM.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/59.Cristiano-Ronaldo-625x302.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/6.2b4038911e58e4941e25052cee52c91f5848b4a5-600x250.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/60.Juventus-star-Cristiano-Ronaldo-deflects-free-kick-into-top-corner-vs-Inter-Milan.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/61.Ronaldo.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/62.20180509_4-razones-para-entender-la-hegemon%C3%ADa-de-la-Juventus-en-Italia.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/63.Juventus-Social-AP.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/64.Ajax.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/65.Juventus.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/66.pes-2020-juventus-gameplay-trailer-exclusive-konami-partnership-1200x450.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/67.2019%2F9%2FChielliniTop.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/68.img-011219-101.onlineBild.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/69.RONALDO.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/7.Ronaldo-Juventus-soccer-1200x600.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/70.Radja-Nainggolan-and-Juan-Cuadrado.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/71.Juventus-Serie-A.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/72.2019%2F8%2Fchiellini30.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/73.lionel-featured_bnzm.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/74.JUVENTUS-ROMA-FOTOGRAMMA-kl8D-1170x507@IlSole24Ore-Web.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/75.Juve%20Social%20Banner_TWITTER%20COVER.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/76.Juventus-1180-x500-4-1407.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/77.img_2083-660x330.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/77.juan-cuadrado-juventus-1024x425.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/78.img_2083-660x330.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/78.juan-cuadrado-juventus-1024x425.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/79.Champions-League-soccer-Atletico-ties-Juventus-on-90th-minute-header.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/8.large_0HbazinU6YDpHkJHoK4ADu01_QU1nZ5AJt3pyqse8GA.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/9.126414.jpg",
+      tags: ["juventus"]
+    },
+    {
+      path: "./Images/gf.py",
+      tags: ["juventus"]
+    },
 ];
 
 // export { data };
