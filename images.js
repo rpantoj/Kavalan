12772803f894eab717a5f2dddcded1e7ebe41795
Ariel Barboza <36430592+xarielx@users.noreply.github.com>
2019-12-03 23:42:18 -0500

    Added Juventus, Dybala Tags and Images

--- images.js
+++ images.js
@@ -352,8 +352,352 @@ const data = [
       tags: ["juventus"]
     },
     {
-      path: "./Images/gf.py",
-      tags: ["juventus"]
+      path: "./Images/1.paulo_dybala_juventus_ac_milan.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/10.2019%2F7%2Fpaulo27.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/11.Paulo-Dybala.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/12.r458343_2_1296x518_5-2.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/13.PA-32798487-800x350_c.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/14.thequint%2F2019-02%2F731484b5-6f2d-4293-92da-64053d14bb52%2FAP19046773437674.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/15.AP19314784917031.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/16.img_5172-660x330.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/17.dybala_6.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/18.juventus-star-paulo-dybala-explains-meaning-behind-gladiator-mask-squawkacom_1266529.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/19.1131207749-746x372.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/2.juventus_dybala-1024x425.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/20.Paulo-Dybala-AP-raw-Juventus-social.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/21.dybala.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/22.paulo-dybala_c7pqe92e27ph12nbr97q0fli0.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/23.image.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/24.1184805254.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/25.Juventus-attacker-Paulo-Dybala-celebrates-scoring-vs-Inter-Milan-e1570525717196-960x450.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/26.Paulo-Dybala-celebrates-Juventus-AFP1.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/27.AP18346736995848.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/28.3F30DBE000000578-0-image-a-4_1492102042759.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/29.041117-ucl-paulo-dybala-celebrate.v-db8b2137b316b510VgnVCM100000d7c1a8c0____.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/3.Dybala_AP.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/30.cristiano-ronaldo-and-paulo-dybala-cropped_5fyqjac1svqq15rf11pjdfgxg.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/31.r609276_2_1296x518_5-2.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/32.1140816482-1-746x372.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/33.juventus-striker-paulo-dybala-scored-3-stunning-lionel-messi-like-goals-on-sunday.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/34.j.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/35.image.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/36.paulo-dybala-juventus-udinese-2018-1024x458.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/37.dybala_e9ee612.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/38.8a8134b93590b507be5aae25aef284d4.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/39.Football-Transfer-Rumors-e1565160308519-800x400.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/4.who-is-paulo-dybala.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/40.Xq-qiyg6n_1256x620__1.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/41.romelu-lukaku.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/42.GettyImages-1148702214-1-970x475.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/43.DYBALA-min.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/44.Paulo-Dybala-1.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/45.1129903116-1206x602.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/46.paulo-dybala_gw86z1m58s0116h16ptaiqj22.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/47.GettyImages-1160513428-970x475.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/48.Paulo-Dybala-Juventus.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/49.dybala-pogba-cropped_1u2pm9yjgm0md1nw61e67v379h.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/5.r580546_1296x518_5-2.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/50.PA-38075689.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/51.Dybala-1200.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/52.juve-293.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/53.r474004_1296x518_5-2.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/54.csportfoto13481152-0-image-a-81_1557834436967-636x280.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/55.2017%2F7%2Fdybala28.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/56.paulo-dybala-01.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/57.paulo-dybala-man-utd-target-returns-to-juventus-for-talks-on-future-football-news.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/58.EAf0dmoWkAEkrpi.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/59.transfer-talk-dybala-among-juve-trio-offered-to-man-united-for-pogba-1200x600.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/6.AP18275684801068.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/60.juve-383.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/61.dybala_ap.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/62.paulo-dybala-juve-997-02.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/63.kisspng-t-shirt-paulo-dybala-juventus-f-c-football-player-aleksandar-mitrovic-5b35d9a5a8c229.5714770015302557816913.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/64.DwuL-aFXQAAIl45.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/65.lg-133409_636854063397848788.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/66.paulo-dybala.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/67.6zH5KWzc6_1256x620__1.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/68.juventus-fc-cristiano-ronaldo-paulo-dybala-facebook-cover.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/69.Dybala-Pogba.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/7.manchester-united-in-talks-with-juventus-for-forward-paulo-dybala.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/70.dybala-1280x531.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/71.EvAEMp1nG_1256x620__1.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/72.dybala-juventus-celebrates-1024x431.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/73.paulo-dybala-juventus.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/74.paulo-dybala-dan-cri_8b2d957.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/75.ccelebritiesfotoCL-COMP-POGBA-750x360.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/76.dybala-01.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/77.224ce8663d6091a75278bcdb61f560d8.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/78.Dzi9pbaWsAA4x-w.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/79.Paulo-Dybala-social-AP.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/8.PA-38906670-e1542292126486-800x350_c.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/80.r533625_1296x518_5-2.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/81.soccer__21_.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/82.9ugj290cjw.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/83.Paulo-Dybala.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/84.Pogba-Dybala-mourinho-e1537092793296.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/85.GettyImages-1052831118-970x475.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/86.ccelebritiesfotoSPORT-PREVIEW-RONALDO-DYBALA-1-750x360.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/87.paulo_dybala_ap_se_queda_en_turxn.jpg",
+      tags: ["juventus", "dybala"]
+    },
+    {
+      path: "./Images/9.2019%2F7%2Fdybala26.jpg",
+      tags: ["juventus", "dybala"]
     },
 ];
 
